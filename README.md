7234a2e9d79044ddaee7af8c6be7a33081d7a9f1
Puran Singh Rana <puransinghrana080@gmail.com>
2025-04-12 11:33:32 +0530

    useReducer

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # web-playground
 
-A collection of small, interactive web projects built using HTML, CSS, and JavaScript.
+A collection of small, interactive web projects built using HTML, CSS, and JavaScript, React.
 
 ## Projects
 
@@ -13,23 +13,34 @@ A collection of small, interactive web projects built using HTML, CSS, and JavaS
    A BLOB URL (URL.createObjectURL(blob)) creates a temporary link to access or download this data in web applications.
 
 3. **Formik_and_Yup--( handles form state, validation, and submission easily )**
+   ✅ Formik
+   Formik is a popular React library that helps you build and manage forms easily. It handles:
+   Form state
+   Validation
+   Submission
+   Error messages
+   It simplifies form code and reduces boilerplate in React apps.
 
-✅ Formik
-Formik is a popular React library that helps you build and manage forms easily. It handles:
-Form state
-Validation
-Submission
-Error messages
-It simplifies form code and reduces boilerplate in React apps.
-
-✅ Yup
-Yup is a JavaScript schema validation library, often used with Formik to define:
-Validation rules (like required fields, min/max values, custom formats)
-Schemas that ensure data shape is correct
-Think of Yup as a tool to define “what your form data should look like.”
+   ✅ Yup
+   Yup is a JavaScript schema validation library, often used with Formik to define:
+   Validation rules (like required fields, min/max values, custom formats)
+   Schemas that ensure data shape is correct
+   Think of Yup as a tool to define “what your form data should look like.”
 
 4. **Higher Order Components**
    A Higher Order Component is a function that takes a component and returns a new component with additional props, logic, or features.
 
 5. **Custom hooks in react**
    A custom hook is a reusable function that encapsulates logic used in multiple components. It allows us to extract common functionality, making the code cleaner and more maintainable.
+6. **useReducer**
+   useReducer is a React Hook used for state management, especially when:
+   ✏️ State logic is complex
+   ✏️ Next state depends on the previous state
+   ✏️ You want a Redux-like pattern (reducer + actions)
+   ✏️ It’s an alternative to useState.
+
+   const [state, dispatch] = useReducer(reducer, initialState);
+
+   🟢 reducer: A function (state, action) => newState
+   🟢 initialState: The starting state
+   🟢 dispatch: A function to send actions to the reducer
---
+++ useReducer/Login.js
@@ -0,0 +1,97 @@
+import React, { useState, useReducer } from "react";
+
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.payload, isValid: action.payload.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.payload, isValid: action.payload.length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.lenght > 6 };
+  }
+  return { value: "", isValid: false };
+};
+
+const Login = (props) => {
+  const [formIsValid, setFormIsValid] = useState(false);
+  //using useReducer
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+  //using useReducer
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({ type: "USER_INPUT", payload: event.target.value });
+    setFormIsValid(event.target.includes("@") && passwordState.isValid);
+  };
+  const passwordChangeHandler = (event) => {
+    dispatchPassword({ type: "USER_INPUT", payload: event.target.value });
+    setFormIsValid(event.target.value.trim().length > 6 && emailState.isValid);
+  };
+
+  const validateEmailHandler = () => {
+    dispatchEmail({ type: "INPUT_BLUR" });
+  };
+  const validatePasswordHandler = () => {
+    dispatchPassword({ type: "INPUT_BLUR" });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault;
+    props.onLogin(emailState.value, passwordState.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={passwordState.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+export default Login;
