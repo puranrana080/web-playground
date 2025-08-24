7d595cee6b2a052c95cd1127d62aa8476c6d8f5e
Puran Singh Rana <puransinghrana080@gmail.com>
2025-08-24 17:32:58 +0530

    add route,nested

--- my-router-app/src/App.js
+++ my-router-app/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import Setting from "./components/Setting";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="account">
+        <Route path="profile" element={<Profile />} />
+         <Route path="setting" element={<Setting />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
