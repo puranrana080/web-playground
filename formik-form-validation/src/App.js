c1e2cab9274d754225f680f078e2a8f1ce09dce4
Puran Singh Rana <puransinghrana080@gmail.com>
2025-08-25 21:44:37 +0530

    add form validation using Yup

--- formik-form-validation/src/App.js
+++ formik-form-validation/src/App.js
@@ -1,4 +1,5 @@
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function App() {
   const formik = useFormik({
@@ -7,13 +8,23 @@ function App() {
       name: "",
       email: "",
       status: "",
-      country:""
+      country: "",
     },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .max(10, "username must be less than or equal to 10 characters")
+        .required("This is required field"),
+      name: Yup.string().required("This field is required"),
+      email: Yup.string()
+        .email("Please provide a valid email")
+        .required("This field is required"),
+      status: Yup.string().required("This field is required"),
+      country: Yup.string().required("This field is required"),
+    }),
     onSubmit: (values) => {
       console.log("Form Submitted", values);
     },
   });
-
   return (
     <div className="App">
       <form onSubmit={formik.handleSubmit}>
@@ -22,26 +33,38 @@ function App() {
           name="username"
           onChange={formik.handleChange}
           value={formik.values.username}
+          onBlur={formik.handleBlur}
           placeholder="Enter UserName"
         />
+        {formik.touched.username && formik.errors.username && (
+          <p style={{ color: "red" }}>{formik.errors.username}</p>
+        )}
         <input
           type="text"
           name="name"
           onChange={formik.handleChange}
           value={formik.values.name}
+          onBlur={formik.handleBlur}
           placeholder="Enter Name"
         />
-        <br/>
+         {formik.touched.name && formik.errors.name && (
+          <p style={{ color: "red" }}>{formik.errors.name}</p>
+        )}
+        <br />
         <input
           type="email"
           name="email"
           onChange={formik.handleChange}
           value={formik.values.email}
+          onBlur={formik.handleBlur}
           placeholder="Enter Email"
-          />
-          <br/>
-          <span>Your Status: </span>
-          
+        />
+         {formik.touched.email && formik.errors.email && (
+          <p style={{ color: "red" }}>{formik.errors.email}</p>
+        )}
+        <br />
+        <span>Your Status: </span>
+
         <input
           type="radio"
           name="status"
@@ -49,28 +72,33 @@ function App() {
           onChange={formik.handleChange}
         />
         <label>Single</label>
-          <input
+        <input
           type="radio"
           name="status"
           value="Commited"
           onChange={formik.handleChange}
         />
         <label>Commited</label>
-         <input
+        <input
           type="radio"
           name="status"
           value="coder"
           onChange={formik.handleChange}
         />
         <label>Coder</label>
-        <br/>
-        <select name="country" onChange={formik.handleChange} >
+         {formik.touched.status && formik.errors.status && (
+          <p style={{ color: "red" }}>{formik.errors.status}</p>
+        )}
+        <br />
+        <select name="country" onChange={formik.handleChange} onBlur={formik.handleBlur}>
           <option value="">Select Country</option>
           <option value="India">India</option>
           <option value="Nepal">Nepan</option>
           <option value="Japan">Japan</option>
-
         </select>
+         {formik.touched.country && formik.errors.country && (
+          <p style={{ color: "red" }}>{formik.errors.country}</p>
+        )}
         <button type="submit">Submit</button>
       </form>
     </div>
