43a76cb89639bbdb9f93d3c2bd84a36350999b54
Puran Singh Rana <puransinghrana080@gmail.com>
2025-09-03 23:11:52 +0530

    add Typography

--- material-ui/src/pages/Create.js
+++ material-ui/src/pages/Create.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Create() {
   return (
     <div>
-      Create page
+     <Typography variant="h6" color="textSecondary" component="h2" gutterBottom>
+      Create a New Note
+     </Typography>
     </div>
-  )
+  );
 }
