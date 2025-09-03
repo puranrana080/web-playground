11d03d80d737b108bea88ed0289c35328d3a60f0
Puran Singh Rana <puransinghrana080@gmail.com>
2025-09-03 22:52:35 +0530

    updated packages+removed error

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,18 +1,14 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Notes />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+      </Routes>
     </Router>
   );
 }
