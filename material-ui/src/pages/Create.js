aac6d581ac2c2a699572b4de1d58b541a7500d36
Puran Singh Rana <puransinghrana080@gmail.com>
2025-09-04 09:50:49 +0530

    makeStyles hook material-ui

--- material-ui/src/pages/Create.js
+++ material-ui/src/pages/Create.js
@@ -2,11 +2,29 @@ import React from "react";
 import { Typography, Button, Container } from "@mui/material";
 import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import {makeStyles} from "@mui/styles"
+
+
+const useStyles = makeStyles({
+  btn:{
+    fontSize:60,
+    backgroundColor:'voilet',
+    '&:hover':{
+      backgroundColor:'blue'
+    }
+  },
+  title:{
+    textDecoration:'underline',
+    marginBottom:'20px'
+  }
+})
 
 export default function Create() {
+  const classes = useStyles()
   return (
     <Container>
       <Typography
+      className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -15,6 +33,7 @@ export default function Create() {
         Create a New Note
       </Typography>
       <Button
+      className={classes.btn}
         type="submit"
         onClick={() => console.log("You clicked me")}
         color="secondary"
