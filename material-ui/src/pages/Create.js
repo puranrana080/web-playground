5bab312b5500c18cbc7a25b8000a641571601a44
Puran Singh Rana <puransinghrana080@gmail.com>
2025-09-06 14:44:54 +0530

    add textfield + form

--- material-ui/src/pages/Create.js
+++ material-ui/src/pages/Create.js
@@ -1,54 +1,76 @@
-import React from "react";
-import { Typography, Button, Container } from "@mui/material";
-import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+import React, { useState } from "react";
+import { Typography, Button, Container, TextField } from "@mui/material";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import { makeStyles } from "@mui/styles";
-
-const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: "voilet",
-    "&:hover": {
-      backgroundColor: "blue",
-    },
-  },
-  title: {
-    textDecoration: "underline",
-    marginBottom: "20px",
-  },
-});
 
 export default function Create() {
-  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+    const [titleErr, setTitleErr] = useState(false);
+  const [detailsErr, setDetailsErr] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleErr(false)
+    setDetailsErr(false)
+    if(title===''){
+      setTitleErr(true)
+    }
+     if(details===''){
+      setDetailsErr(true)
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    } 
+  };
+
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant="h6"
-        color="secondary"
+        color="textSecondary"
         component="h2"
         gutterBottom
       >
         Create a New Note
       </Typography>
-      <Button
-        className={classes.btn}
-        type="submit"
-        onClick={() => console.log("You clicked me")}
-        color="primary"
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        Submit
-      </Button>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          sx={{ mt: 2, mb: 2, display: "block" }}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleErr}
+        ></TextField>
+        <TextField
+          sx={{ mt: 2, mb: 2, display: "block" }}
+          onChange={(e) => setDetails(e.target.value)}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          multiline
+          rows={4}
+          required
+          error={detailsErr}
+        ></TextField>
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+
       {/* icons */}
       <br />
-      <AddCircleOutlinedIcon />
-      <AddCircleOutlinedIcon color="secondary" fontSize="large" />
-      <AddCircleOutlinedIcon color="secondary" fontSize="small" />
-      <AddCircleOutlinedIcon color="action" fontSize="small" />
-      <AddCircleOutlinedIcon color="error" fontSize="small" />
-      <AddCircleOutlinedIcon color="disabled" fontSize="small" />
     </Container>
   );
 }
