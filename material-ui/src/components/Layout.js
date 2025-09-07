5c94bcc193c79bbecfc07edc72aacc2eeb65effe
Puran Singh Rana <puransinghrana080@gmail.com>
2025-09-07 15:34:00 +0530

    Drawer List Items

--- material-ui/src/components/Layout.js
+++ material-ui/src/components/Layout.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
-import { Drawer, Typography, Box } from "@mui/material";
+import { Drawer, Typography, Box ,List,ListItem,ListItemIcon,ListItemText, ListItemButton} from "@mui/material";
+import { AddCircleOutline, SubjectOutlined } from "@mui/icons-material";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const drawerWidth = 240;
 
@@ -10,6 +12,18 @@ const PageST = styled("div")({
 });
 
 const Layout = ({ children }) => {
+    const navigate = useNavigate()
+    const location =useLocation()
+    const menuItems = [{
+        text:"My Notes",
+        icon:<SubjectOutlined color="secondary"/>,
+        path:'/'
+    },
+{
+        text:"Create Notes",
+        icon:<AddCircleOutline color="secondary"/>,
+        path:'/create'
+    }]
   return (
     <Box sx={{ display: "flex" }}>
       {/* App Bar */}
@@ -19,10 +33,10 @@ const Layout = ({ children }) => {
         variant="permanent"
         anchor="left"
         sx={{
-          width: 240,
+          width: drawerWidth,
           flexShrink: 0,
           "& .MuiDrawer-paper": {
-            width: 240,
+            width: drawerWidth,
             boxSizing: "border-box",
           },
         }}
@@ -30,6 +44,26 @@ const Layout = ({ children }) => {
         <div>
           <Typography variant="h5">Ninja Notes</Typography>
         </div>
+        {/* list /links */}
+        <List>
+        {menuItems.map(item=>(
+            <ListItem key={item.text} onClick={()=>navigate(item.path)} className={location.pathname===item.path?"active":""} sx={{
+            cursor: "pointer",
+            "&.active": {
+              backgroundColor: "#d3d3d3",
+              fontWeight: "bold",
+              borderRight:'5px solid red'
+            },
+          }} >
+                <ListItemButton>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+                </ListItemButton>
+            </ListItem>
+        ))}
+
+        </List>
+       
       </Drawer>
 
       <PageST>{children}</PageST>
