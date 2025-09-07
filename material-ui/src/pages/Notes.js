6b826f1eb98b74802ec99f8b7d990ef4530869e7
Puran Singh Rana <puransinghrana080@gmail.com>
2025-09-07 17:28:55 +0530

    added react-masonry-css

--- material-ui/src/pages/Notes.js
+++ material-ui/src/pages/Notes.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Grid  } from '@mui/material'
+import { Container } from '@mui/material'
 import NoteCard from '../components/NoteCard'
+import Masonry from "react-masonry-css"
 
 export default function Notes() {
   const [notes,setNotes] =useState([])
@@ -18,18 +19,35 @@ export default function Notes() {
    const newNotes = notes.filter(note=>note.id!==id)
    setNotes(newNotes)
   }
+
+  const breakpoints = {
+    default:3,
+    1100:2,
+    700:1
+  }
   return (
     <Container>
-      <Grid container spacing={3}>
-
+      {/* <Grid container spacing={3}>
       {notes.map(note=>(
         <Grid key={note.id} size={{xs:12,md:6,lg:4}}>
           <NoteCard note={note} handleDelete={handleDelete} />
         </Grid>
-
       ))}
 
-      </Grid>
+      </Grid> */}
+      <Masonry 
+      breakpointCols={breakpoints}
+      className='my-masonry-grid'
+      columnClassName='my-masonry-grid_column'
+      >
+
+       {notes.map(note=>(
+        <div key={note.id} >
+          <NoteCard note={note} handleDelete={handleDelete} />
+        </div>
+         ))}
+      </Masonry>
+
     </Container>
   )
 }
